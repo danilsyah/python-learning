a79f8d55bcde2bdd5b250ee776a94a184d2e55a8
danil syah <danilsyaharihardjo@gmail.com>
2020-05-14 08:54:11 +0700

    menambahkan file 02-block-indentation.py dan 03-datatypes.py

--- 01-Chapter-intro/01-variable.py
+++ 01-Chapter-intro/01-variable.py
@@ -21,3 +21,15 @@ a = b = c = 1
 print(a,b,c)
 b = 2
 print(a,b,c)
+
+x = y = [7, 8, 9]
+x = [13, 8, 9]
+print(y)
+
+x = y = [7, 8, 9]
+x[0] = 13
+print(y)
+
+x = [1, 2, [3, 4, 5], 6, 7] #this is nested list
+print(x[2])
+print(x[2][1])
---
+++ 01-Chapter-intro/03-datatypes.py
@@ -0,0 +1,21 @@
+# belajar tipe data
+
+# beberapa jenis tipe data
+#  1. numbers : 
+#  int
+angka = 10
+print(angka ,"type = ", type(angka))
+
+# float
+pecahan = 3.14
+print(pecahan, "type = ", type(pecahan))
+
+# complex
+a = 2 + 1j
+print(a, " type = ", type(a))
+
+# string
+nama_depan = "danil"
+nama_belakang = "syah arihardjo"
+full_name = nama_depan + " "+ nama_belakang
+print(full_name, "type = ", type(full_name))
