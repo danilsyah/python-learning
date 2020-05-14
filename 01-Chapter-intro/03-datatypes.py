13a07e4dc63747eba10ae3b9f05bc57f01f29e81
danil syah <danilsyaharihardjo@gmail.com>
2020-05-14 09:56:53 +0700

    update file 03-datatypes.py dan add file 04-collection-types.py

--- 01-Chapter-intro/03-datatypes.py
+++ 01-Chapter-intro/03-datatypes.py
@@ -19,3 +19,40 @@ nama_depan = "danil"
 nama_belakang = "syah arihardjo"
 full_name = nama_depan + " "+ nama_belakang
 print(full_name, "type = ", type(full_name))
+
+i = "3"
+print(isinstance(i, int))
+if isinstance(i, int):
+    i += 1
+elif isinstance(i, str):
+    print("type :", type(i))
+    i = int(i)
+    i += 1
+print(i)
+
+#  konversi type data
+# int
+a = '123'
+b = int(a)
+print(a, "type = ", type(a), "konversi ke ", type(b))
+
+# float
+a = '123.1234'
+b = float(a)
+print(a, "type = ", type(a), "konversi ke ", type(b))
+
+#  konversi ke list, set, tuple
+a = 'hello'
+print(list(a))
+print(set(a))
+print(tuple(a))
+
+# mutable : tipe data variabel yang bisa di ubah
+# immutable : tipe data variable yang tidak bisa di ubah
+def f(m):
+    m.append(3) #menambahkan number ke list
+    m.append(5)
+    return m
+x = [1, 2]
+print(f(x))
+
---
+++ 01-Chapter-intro/04-collection-types.py
@@ -0,0 +1,24 @@
+# List
+int_list = [1, 2, 3]
+string_list = ['danil', 'syah']
+empty_list = []
+mixed_list = [1, 'abc', True, 2.34, None]
+nested_list = [['a','b','c'], [1, 2, 3]]
+print(int_list)
+print(string_list)
+print(empty_list)
+print(mixed_list)
+print(nested_list)
+
+names = ['Danil','Khalinda','Fika','Haykal']
+print(names[2])
+print(names[1])
+print(names[-1])
+print(names[-3])
+print(names)
+names[0] = 'rizal'
+print(names)
+names.append("icih") # menambahkan data ke list
+print(names)
+names.insert(2, "udin")
+print(names)
