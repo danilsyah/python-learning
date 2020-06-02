cc1fa4a2d9dd5df34af078d182c9ae56755bf9d6
danil syah <danilsyaharihardjo@gmail.com>
2020-06-02 13:32:31 +0700

    add course latihan komparasi & logika

---
+++ 09-Latihan Komparasi & Logika/Main.py
@@ -0,0 +1,11 @@
+# membuat gabungan area rentang dari angka
+
+inputUser = float(
+    input("Masukan Angka yang bernilai kurang dari 3 atau lebih besar dari 10 : "))
+# memeriksa angka kurang dari 3
+isKurangDari = (inputUser < 3)
+print(isKurangDari)
+
+# memeriksa angka lebih dari 10
+isLebihDari = (inputUser > 10)
+print(isLebihDari)
